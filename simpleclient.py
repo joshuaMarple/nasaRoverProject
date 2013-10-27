55ef1b71c529ed429a7c1cca108195b31ce5b6d8
CCSL-Students <students@CCSL-1156-02.(none)>
2013-10-26 22:11:47 -0500

    added simpleclient

---
+++ simpleclient.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import sys
+import socket
+import thread
+import time
+
+
+######## Main loop. ########
+
+def dodisthang():
+	# try:
+	print "SimpleChat starting."
+	IP = str(raw_input("IP:"))
+	port = int(raw_input("port:"))
+	print checkForServer(IP,port)
+	client(IP,port)
+
+
+                        
+#### Other functions go here.######
+def checkForServer(IP, port):
+	if sendMessage(port, IP, "foo") == "boop":
+		return True
+	else: 
+		return False
+
+def sendMessage(port, ip, message):
+	try:
+		#setup
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
+		if message == '':
+			message = 'Blank Message.'           
+		s.connect((ip, port))
+		s.sendall(message)
+		data =  s.recv(1024)
+		s.close
+		return data
+	except:
+		print "No response."
+		return "fish"
+
+
+def client(ip, port):
+	while True:
+		print "Client up."
+		message = str(raw_input(">>:"))
+		sendMessage(port, ip, message)
+	except:
+		print 'unknown error'	
+		break
+
+
+
+
+if __name__ == '__main__':
+	dodisthang()    
